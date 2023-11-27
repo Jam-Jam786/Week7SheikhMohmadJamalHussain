0da4d2f6da33e226bf03e8682f46c4d3e0001d7e
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-27 16:00:32 +0000

    Created the Divs for every section and then added css to adjust it

---
+++ interactiv_glossary/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Button from './InteractiveGlossary.jsx'
+
+function App() {
+  return (
+
+// const
+// title
+// description
+
+
+
+
+
+
+    <div className="App">
+      <header className="App-header">
+
+
+        <div className='grid-container'>
+
+
+          <div className='card-container'>
+            <div className='card-information'>Card</div>
+          </div>
+
+          <div className='button-container'>
+            <div className='buttons-column'>Buttons</div>
+          </div>
+
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ interactiv_glossary/src/InteractiveGlossary.jsx
@@ -0,0 +1,20 @@
+import './InteractiveGlossary.css'
+
+
+function Button(){
+    return(
+        <div className='Buttons'>
+            
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+export default Button;
